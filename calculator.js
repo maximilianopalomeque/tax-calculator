903b79b098be6f25c92dfafb4293ccf1e3d9d620
max <maximilianopalomeque@gmail.com>
2021-12-06 07:24:29 -0300

    currency functionality added, now usd or pesos can be selected

--- calculator.js
+++ calculator.js
@@ -1,45 +1,14 @@
 const nintendoTaxes = 1.43;
 const xboxSteamTaxes = 1.65;
+const usdTaxes = 1.65;
+const usdApiUrl = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
 
 var recentSearches = []; // array to store recent calculated games
 
-let button = document.getElementById("buttonCalculate");
-
-button.addEventListener("click", function(){
-  let selectedPlatform = document.getElementById("platform").value;
-  let gameName = document.getElementById("gameName").value;
-  let gamePrice = document.getElementById("value").value; // get price without taxes
-
-  if (!(selectedPlatform === "Plataforma")){
-    if ((gamePrice > 0) && (gamePrice < 200000)){
-      let result = calculateTax (gamePrice, selectedPlatform); // return the final value
-      document.getElementById("displayResult").innerHTML = gameName + " costara con impuestos: " + result + " pesos."; // display result
-
-      let actualGame = {
-        platform: selectedPlatform,
-        name: gameName,
-        price: gamePrice,
-        priceWithTaxes: result
-      };
-      recentSearches.unshift(actualGame); // store the game in the beggining of array
-      displayArray(); // add first element of the array in the table
-    }
-    else {
-      document.getElementById("displayResult").innerHTML = "Ingrese un precio valido"; 
-    }
-  }
-  else {
-    document.getElementById("displayResult").innerHTML = "Seleccionar plataforma"; 
-  }
-})
-
-function calculateTax(gamePrice, selectedPlatform){
-  if (selectedPlatform === "Nintendo") {
-    return (gamePrice * nintendoTaxes).toFixed(2);
-  }
-  else {
-    return (gamePrice * nintendoTaxes).toFixed(2);
-  }
+async function getUsdApiData(){
+  const response = await fetch(usdApiUrl);
+  var usdObject = await response.json();
+  return usdObject;
 }
 
 function displayArray(){
@@ -57,24 +26,57 @@ function displayArray(){
   td4.innerHTML = recentSearches[0].priceWithTaxes;
 }
 
-/*
-let table = document.getElementById("recent");
-let tr = document.createElement("tr");
+function createObjectGame (selectedPlatform, gameName, gamePrice, result) {
+  let actualGame = {
+    platform: selectedPlatform,
+    name: gameName,
+    price: gamePrice,
+    priceWithTaxes: result
+  };
+  recentSearches.unshift(actualGame);
+}
 
-  let td1 = document.createElement("td");
-  let td2 = document.createElement("td");
-  let td3 = document.createElement("td");
-  let td4 = document.createElement("td");
+let button = document.getElementById("buttonCalculate");
+button.addEventListener("click", function(){
+  let selectedPlatform = document.getElementById("platform").value;
+  let gameName = document.getElementById("gameName").value;
+  let gamePrice = document.getElementById("value").value; // get price without taxes
+  let selectedCurrency = document.getElementById("currency").value;
+  let result = 0;
 
-  td1.appendChild(document.createTextNode(recentSearches[0].platform));
-  td2.appendChild(document.createTextNode(recentSearches[0].name));
-  td3.appendChild(document.createTextNode(recentSearches[0].price));
-  td4.appendChild(document.createTextNode(recentSearches[0].priceWithTaxes));
-
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-  tr.appendChild(td4);
-
-  table.appendChild(tr);
-*/
+  if (!(selectedPlatform === "Seleccione plataforma")){
+    if ((gamePrice > 0) && (gamePrice < 200000)){
+      if (!(selectedCurrency === "Seleccione moneda")){
+        if (selectedCurrency === "Pesos"){
+          if (!(selectedPlatform === "PlayStation")){
+            selectedPlatform === "Nintendo" ? result = (gamePrice * nintendoTaxes).toFixed(2) : result = (gamePrice * xboxSteamTaxes).toFixed(2)
+            createObjectGame (selectedPlatform, gameName, gamePrice, result);
+            document.getElementById("displayResult").innerHTML = gameName + " costara con impuestos: " + result + " pesos."; // display result
+            displayArray(); // display the first element of the array in the table
+          }
+          else {
+            document.getElementById("displayResult").innerHTML = "PlayStation no tiene tienda en pesos, seleccione USD";
+          }
+        }
+        else { // USD is selected
+          getUsdApiData().then(data => {
+            result = ((gamePrice * parseFloat(data[0].casa.venta)) * usdTaxes).toFixed(2); // (gameprice * dolar oficial) * usd taxes
+            createObjectGame (selectedPlatform, gameName, gamePrice, result);
+            document.getElementById("displayResult").innerHTML = gameName + " costara con impuestos: " + result + " pesos."; // display result
+            displayArray();
+          });
+        }
+      }
+      else {
+        document.getElementById("displayResult").innerHTML = "Seleccione moneda";   
+      }
+    }
+    else {
+      document.getElementById("displayResult").innerHTML = "Ingrese un precio valido"; 
+    }
+  }
+  else {
+    document.getElementById("displayResult").innerHTML = "Seleccionar plataforma"; 
+  }
+  
+})
